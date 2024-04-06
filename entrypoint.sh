233d14f7923a3e3ccb4802814013e4cb163a33e9
Gaganpreet Kaberwal <g.kaberwal@mail.utoronto.com>
2024-04-05 20:50:02 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,12 @@
 
 cd /app
 
-# Create the Rails production DB on first run
+# Create the Rails production DB if not created
 RAILS_ENV=production bundle exec rake db:create
 
 # update database schema
 RAILS_ENV=production bundle exec rake db:migrate
 
-# Do some protective cleanup
-# > log/production.log
 # Fixes a glitch with the pids directory by removing the server.pid file on execute.
 rm -f tmp/pids/server.pid
 
